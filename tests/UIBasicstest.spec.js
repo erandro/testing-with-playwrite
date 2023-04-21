4cedb545d4b07bab4418a67e8822015f062621c9
Eran <erandro@gmail.com>
2023-04-21 13:20:19 -0500

    added locator logic in testing ui elements

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,8 +4,20 @@ test('Browser context playwright test', async ({browser}) => {
 
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://www.cnn.com/");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
     console.log(await page.title());
+
+    // locate element with id
+    await page.locator('#username').type('rahulshetty');
+    // locate element with attribute
+    await page.locator("[type='password']").type('learning');
+    // locate element with class
+    await page.locator('.btn-md').click();
+
+    // wait until this locator is on the page
+    const alert = page.locator("[style*='block']");
+    await expect(alert).toContainText('Incorrect username/password')
+    console.log(await alert.textContent());
 });
 
 test('Page playwright test', async ({page}) => {
