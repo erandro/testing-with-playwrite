3883a6ad2e0af8830909d26f1a0037e7bfbaefc9
Eran <erandro@gmail.com>
2023-04-21 14:21:47 -0500

    dealing with allTextContents not being auto-waiting

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -7,17 +7,32 @@ test('Browser context playwright test', async ({browser}) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise");
     console.log(await page.title());
 
-    // locate element with id
-    await page.locator('#username').type('rahulshetty');
-    // locate element with attribute
-    await page.locator("[type='password']").type('learning');
-    // locate element with class
-    await page.locator('.btn-md').click();
+    // enter wrong username
 
+    // locate element with id
+    const userName = page.locator('#username');
+    await userName.type('rahulshetty');
+    // locate element with attribute
+    const password = page.locator("[type='password']");
+    password.type('learning');
+    // locate element with class
+    const signInButton = page.locator('.btn-md');
+    await signInButton.click();
     // wait until this locator is on the page
     const alert = page.locator("[style*='block']");
     await expect(alert).toContainText('Incorrect username/password')
     console.log(await alert.textContent());
+
+    // enter correct username
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signInButton.click();
+    const cardsTitles = page.locator(".card-body a");
+    // the next line is only making the code wait (not doing anything else). Whithout it allCardsTitles will fire imidiatly, since allTextContents() isn't an "auto-waiting" function, and will return [].
+    await cardsTitles.nth(0).textContent();
+    const allCardsTitles = await cardsTitles.allTextContents();
+    console.log(allCardsTitles);
+
 });
 
 test('Page playwright test', async ({page}) => {
