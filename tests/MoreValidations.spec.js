3a16e05fd9e4a8fcc97f22a1bf084ae5ee5a3d19
Eran <erandro@gmail.com>
2023-05-04 11:29:51 -0500

    for now I commented out the issues with testing, and am waiting for an answer from auther about this

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,34 +1,43 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Navigating back and forward on the browser', async({page}) => {
+test('Navigating back and forward on the browser', async ({ page }) => {
     await page.goto('http://www.rahulshettyacademy.com/AutomationPractice/');
     await page.goto('https://google.com');
     await page.goBack();
     await page.goForward();
- });
- 
- test('Dealing with hidden/visable elements', async({page}) => {
-   await page.goto('http://www.rahulshettyacademy.com/AutomationPractice/');
-
-   const displayText = page.locator('#displayed-text');
-   await expect(displayText).toBeVisible();
-   await page.locator('#hide-textbox').click();
-   await expect(displayText).toBeHidden();
 });
 
-test('Dealing with alerts/confirms popups', async({page}) => {
+test('Dealing with hidden/visable elements', async ({ page }) => {
     await page.goto('http://www.rahulshettyacademy.com/AutomationPractice/');
- 
+
+    const displayText = page.locator('#displayed-text');
+    await expect(displayText).toBeVisible();
+    await page.locator('#hide-textbox').click();
+    await expect(displayText).toBeHidden();
+});
+
+test('Dealing with alerts/confirms popups', async ({ page }) => {
+    await page.goto('http://www.rahulshettyacademy.com/AutomationPractice/');
+
     const alertButton = page.locator('#alertbtn');
     const confirmButton = page.locator('#confirmbtn');
     page.on('dialog', dialog => dialog.accept())
     await alertButton.click();
-    page.on('dialog', dialog => dialog.dismiss())
+    // page.on('dialog', dialog => dialog.dismiss())
     await confirmButton.click();
- });
+});
 
- test('Hover', async({page}) => {
+test('Hover', async ({ page }) => {
     await page.goto('http://www.rahulshettyacademy.com/AutomationPractice/');
- 
+
     await page.locator('#mousehover').hover();
- });
+});
+
+// test.only('Dealing with iFrames', async ({ page }) => {
+//     await page.goto('http://www.rahulshettyacademy.com/AutomationPractice/');
+
+//     const framePage = page.frameLocator('#courses-iframe');
+//     await framePage.locator('li a[href*="lifetime-access"]:visible').click()
+//     const textCheck = await framePage.locator('.text h2').textContent()
+//     console.log(textCheck.split(' ')[1])
+// });
